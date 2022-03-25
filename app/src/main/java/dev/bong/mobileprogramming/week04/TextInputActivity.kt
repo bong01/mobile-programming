dafc9eb01c81971b2182961eb2116b94476432bd
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-03-25 21:31:15 +0900

    Week04: TextInputLayout

---
+++ app/src/main/java/dev/bong/mobileprogramming/week04/TextInputActivity.kt
@@ -0,0 +1,28 @@
+package dev.bong.mobileprogramming.week04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import dev.bong.mobileprogramming.R
+
+class TextInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_input)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        val textInputLayout = findViewById<TextInputLayout>(R.id.textInputLayout)
+        val emailText = findViewById<TextInputEditText>(R.id.emailText)
+        emailText.addTextChangedListener {
+            if (it.toString().contains('@')) {
+                textInputLayout.error = null
+            } else {
+                textInputLayout.error = "이메일 형식이 올바르지 않습니다."
+            }
+        }
+    }
+}
