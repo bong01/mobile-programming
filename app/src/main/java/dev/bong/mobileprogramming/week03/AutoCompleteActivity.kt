45ef017a7c1fa7600dc38dcc7f0ad85ad91e41ce
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-04-19 12:01:29 +0900

    Week03: AutoCompleteTextView(주석 추가)

--- app/src/main/java/dev/bong/mobileprogramming/week03/AutoCompleteActivity.kt
+++ app/src/main/java/dev/bong/mobileprogramming/week03/AutoCompleteActivity.kt
@@ -23,6 +23,8 @@ class AutoCompleteActivity : AppCompatActivity() {
     }
 
     private fun initLayout() {
+        //AutoCompleteTextView에 적용할 어댑터 정의
+        //인자로 ArrayList 전달
         val adapter = ArrayAdapter<String>(
             this,
             android.R.layout.simple_dropdown_item_1line,
@@ -32,20 +34,29 @@ class AutoCompleteActivity : AppCompatActivity() {
         val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
         val multiAutoCompleteTextView = findViewById<MultiAutoCompleteTextView>(R.id.multiAutoCompleteTextView)
 
+        //AutoCompleteTextView에 어댑터 정의
         autoCompleteTextView.setAdapter(adapter)
+
+        //클릭 리스너
         autoCompleteTextView.setOnItemClickListener { adapterView, view, i, l ->
             val item = adapterView.getItemAtPosition(i).toString()
             Toast.makeText(this, item + " 선택함", Toast.LENGTH_SHORT).show()
         }
 
+        //strings.xml에서 정의한 Array 불러오기
         val greenjoa = resources.getStringArray(R.array.countries_array)
+
+        //어댑터 정의
+        //인자로 Array 전달
         val adapter2 = ArrayAdapter<String>(
             this,
             android.R.layout.simple_dropdown_item_1line,
             greenjoa
         )
 
+        //어댑터 적용
         multiAutoCompleteTextView.setAdapter(adapter2)
+        //MultiAutoCompleteTextView일 경우 구분자 설정하기
         multiAutoCompleteTextView.setTokenizer(MultiAutoCompleteTextView.CommaTokenizer())
     }
 }
