543cc317412a1a0fc1248a7f3007c1baa93578c5
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-04-19 12:03:35 +0900

    Week04: TextWatcher(주석 추가)

--- app/src/main/java/dev/bong/mobileprogramming/week04/TextWatcherActivity.kt
+++ app/src/main/java/dev/bong/mobileprogramming/week04/TextWatcherActivity.kt
@@ -27,35 +27,42 @@ class TextWatcherActivity : AppCompatActivity() {
     private fun initLayout() {
         val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
 
+        //MutableList로 어댑터 정의
         adapter = ArrayAdapter<String>(
             this,
             android.R.layout.simple_dropdown_item_1line,
             countries
         )
+
+        //어댑터 적용
         autoCompleteTextView.setAdapter(adapter)
+        //클릭 리스너
         autoCompleteTextView.setOnItemClickListener { adapterView, view, i, l ->
             val item = adapterView.getItemAtPosition(i).toString()
             Toast.makeText(this, "선택된 나라 : $item", Toast.LENGTH_SHORT).show()
         }
 
         val multiAutoCompleteTextView = findViewById<MultiAutoCompleteTextView>(R.id.multiAutoCompleteTextView)
+        //구분자 설정
         multiAutoCompleteTextView.setTokenizer(MultiAutoCompleteTextView.CommaTokenizer())
+        //어댑터 적용
         multiAutoCompleteTextView.setAdapter(adapter)
 
         val editText = findViewById<EditText>(R.id.editText)
         val button = findViewById<Button>(R.id.button)
 
         /**
-         * afterTextChanged 오버라이딩
+         * 오버라이딩 방법 3가지
          */
+
+        // 1. afterTextChanged 오버라이딩 (기본)
         editText.addTextChangedListener {
             val str = it.toString()
+            //텍스트가 있으면 버튼 활성화
             button.isEnabled = str.isNotEmpty()
         }
 
-        /**
-         * 필요한 메서드 오버라이딩
-         */
+        // 2. 필요한 메서드 오버라이딩
 //        editText.addTextChangedListener(
 //            afterTextChanged = {
 //                val str = it.toString()
@@ -63,9 +70,7 @@ class TextWatcherActivity : AppCompatActivity() {
 //            }
 //        )
 
-        /**
-         * 익명 클래스 사용
-         */
+        // 3. 익명 클래스 사용
 //        editText.addTextChangedListener(object : TextWatcher {
 //            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 //
@@ -83,7 +88,9 @@ class TextWatcherActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             adapter.add(editText.text.toString())
+            //데이터 변경 시 알려줘야 함
             adapter.notifyDataSetChanged()
+            //EditText의 text 지우기
             editText.text.clear()
         }
 
